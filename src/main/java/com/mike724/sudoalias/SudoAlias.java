6f0359a1219bf8e0acebc09c943afb3d101ffe7c
Michael Meyer <michaelmeyer724@gmail.com>
2013-02-03 20:32:20 -0500

    Remove debug message, add copyright headers to all source files

--- src/main/java/com/mike724/sudoalias/SudoAlias.java
+++ src/main/java/com/mike724/sudoalias/SudoAlias.java
@@ -1,3 +1,19 @@
+/*
+    This file is part of SudoAlias.
+
+    SudoAlias is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SudoAlias is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SudoAlias.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.mike724.sudoalias;
 
 import java.util.ArrayList;
